ccd75667dfaf68f14eff5595e9b152bf35b04c71
Naruto777549649 <sikrar026@gmail.com>
2025-04-03 18:09:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 # Apne Bot Token, API_ID, API_HASH ko set karein.
 # WELCOME_IMAGE: Aapke welcome message ke liye image ka path.
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-API_ID = 123456         # Apna API_ID yahan daalein
-API_HASH = "YOUR_API_HASH_HERE"
+BOT_TOKEN = "7615533568:AAF3TfY956Z1YH31HX_CzBvT2oWTcm800xk"
+API_ID = 25698862         # Apna API_ID yahan daalein
+API_HASH = "7d7739b44f5f8c825d48cc6787889dbc"
 
 # Welcome image file (ensure this file exists in the project folder)
-WELCOME_IMAGE = "welcome.jpg"
+WELCOME_IMAGE = "https://iili.io/3uMCKut.md.jpg"
